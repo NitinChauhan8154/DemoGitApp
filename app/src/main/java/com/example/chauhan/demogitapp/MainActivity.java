28641cffc55b3746736912042c2d21c7640f79e6
NitinChauhan2016 <nitinchauhan2016@gmail.com>
2018-05-15 15:29:00 +0530

    nitin

--- app/src/main/java/com/example/chauhan/demogitapp/MainActivity.java
+++ app/src/main/java/com/example/chauhan/demogitapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.chauhan.demogitapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvHello = findViewById(R.id.tvHello);
+        tvHello.setTextColor(R.color.colorPrimary);
+
     }
 }
